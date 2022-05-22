ef9e894a59cef20f100ef44c54e3f1d2cde1f9c3
Dwaine Gallimore <dwainegallimore@users.noreply.github.com>
2022-05-22 18:35:31 +0100

    Minor updates

--- js/charts.js
+++ js/charts.js
@@ -2,7 +2,6 @@ updateData();
 
 async function updateData() {
     const datapoints = await getCurrentData();
-	console.log(datapoints);
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
