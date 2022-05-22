9200c21d1ec0d332dc1bfb3fcd992a28acf5b135
Dwaine Gallimore <dwainegallimore@users.noreply.github.com>
2022-05-22 18:32:24 +0100

    Insert console lot to see what is in the datapoints array.

--- js/charts.js
+++ js/charts.js
@@ -2,6 +2,7 @@ updateData();
 
 async function updateData() {
     const datapoints = await getCurrentData();
+	console.log(datapoints);
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
